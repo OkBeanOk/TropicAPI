6e88ac89adfd2e61d5d66212ee6c069019e4985b
MigrateAccount <developingplugins@gmail.com>
2025-12-16 10:34:45 +0000

    HikariCP has been integrated. Ver bump.

---
+++ src/main/java/com/okbeanok/tropicapi/api/database/SQLiteConnector.java
@@ -0,0 +1,91 @@
+package com.okbeanok.tropicapi.api.database;
+
+import com.okbeanok.tropicapi.TropicAPI;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class SQLiteConnector implements DatabaseConnector {
+
+    private final TropicAPI tropicAPI;
+    private HikariDataSource hikari;
+    private final AtomicInteger openConnections;
+    private final Object lock;
+
+    public SQLiteConnector(TropicAPI tropicAPI, String hostname, int port, String database, String username, String password, boolean useSSL, int poolSize) {
+        this.tropicAPI = tropicAPI;
+        this.openConnections = new AtomicInteger();
+        this.lock = new Object();
+
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:mysql://" + hostname + ":" + port + "/" + database + "?useSSL=" + useSSL + "&allowPublicKeyRetrieval=true&serverTimezone=UTC&useUnicode=true&characterEncoding=UTF-8");
+        config.setUsername(username);
+        config.setPassword(password);
+        config.setMaximumPoolSize(poolSize);
+
+        try { // Try to use the new driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e1) {
+            try { // Otherwise fallback to the old one or just ignore if it still can't be found
+                Class.forName("com.mysql.jdbc.Driver");
+            } catch (ClassNotFoundException ignored) { }
+        }
+
+        try {
+            this.hikari = new HikariDataSource(config);
+        } catch (Exception ex) {
+            this.tropicAPI.getLogger().severe("Failed to connect to the MySQL server. Are your credentials correct?");
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public void closeConnection() {
+        this.hikari.close();
+    }
+
+    @Override
+    public void connect(ConnectionCallback callback) {
+        this.openConnections.incrementAndGet();
+        try (Connection connection = this.hikari.getConnection()) {
+            callback.accept(connection);
+        } catch (SQLException ex) {
+            this.tropicAPI.getLogger().severe("An error occurred executing a MySQL query: " + ex.getMessage());
+            ex.printStackTrace();
+        } finally {
+            int open = this.openConnections.decrementAndGet();
+            synchronized (this.lock) {
+                if (open == 0)
+                    this.lock.notify();
+            }
+        }
+    }
+
+    @Override
+    public void connect(ConnectionCallback callback, boolean useTransaction) {
+        this.connect(callback);
+    }
+
+    @Override
+    public Connection connect() throws SQLException {
+        return this.hikari.getConnection();
+    }
+
+    @Override
+    public Object getLock() {
+        return this.lock;
+    }
+
+    @Override
+    public boolean isFinished() {
+        return this.openConnections.get() == 0;
+    }
+
+    @Override
+    public void cleanup() {
+
+    }
+
+}
